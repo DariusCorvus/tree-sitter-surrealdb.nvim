48a4ecc85be4162a2bcf983152340fe90ad3fa7c
Darius <61415118+DariusCorvus@users.noreply.github.com>
2023-04-23 05:18:57 +0200

    Create init.lua

---
+++ lua/tree-sitter-surrealdb/init.lua
@@ -0,0 +1,50 @@
+vim.api.nvim_command([[
+augroup SurqlFiletype
+  autocmd BufRead,BufNewFile *.surql :set filetype=surql
+augroup END
+]])
+
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.surrealdb = {
+	install_info = {
+		url = "https://github.com/DariusCorvus/tree-sitter-surrealdb.git",
+		files = { "src/parser.c" },
+		branch = "main",
+	},
+	filetype = "surql",
+}
+
+local highlights_scm = [[
+(keyword) @keyword
+(string) @string
+(number) @number
+(punctuation) @punctuation
+(operator) @operator
+(variable) @type
+(constant) @constant.builtin
+(function) @function
+(bool) @boolean
+(nothing) @type
+(comment) @comment
+(record) @type
+(function) @function
+(property) @field
+(identifier) @identifier
+(casting) @conceal
+(duration) @number
+]]
+
+local runtime_path = vim.api.nvim_list_runtime_paths()[1]
+if vim.fn.isdirectory(runtime_path .. "/queries") == 0 then
+	vim.fn.mkdir(runtime_path .. "/queries")
+end
+if vim.fn.isdirectory(runtime_path .. "/queries/surrealdb") == 0 then
+	vim.fn.mkdir(runtime_path .. "/queries/surrealdb")
+end
+
+if vim.fn.isdirectory(runtime_path .. "/queries/surrealdb") == 1 then
+	local highlights_file = io.open(runtime_path .. "/queries/surrealdb/highlights.scm", "w")
+	io.output(highlights_file)
+	io.write(highlights_scm)
+	io.close(highlights_file)
+end
