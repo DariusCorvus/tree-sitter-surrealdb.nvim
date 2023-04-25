ccf776be7e77e3a9552f3383d040476d7f6ed9d3
Darius <dariuscorvus@pm.me>
2023-04-25 09:32:26 +0200

    fix(inline): identifier highlighting

--- lua/tree-sitter-surrealdb/init.lua
+++ lua/tree-sitter-surrealdb/init.lua
@@ -30,12 +30,13 @@ local function setup()
 	(record) @type
 	(function) @function
 	(property) @field
-	(identifier) @text.emphasis
+	(identifier) @constant @text.emphasis (#set! "priority" 120)
 	(casting) @conceal
 	(duration) @number
 	(type) @type
+	(delimiter) @punctuation.delimiter
 	]]
-	
+
 	local injections_scm = [[
 	(scripting_content) @javascript
 	]]
@@ -53,12 +54,12 @@ local function setup()
 		io.output(highlights_file)
 		io.write(highlights_scm)
 		io.close(highlights_file)
-		
+
 		local injections_file = io.open(runtime_path .. "/queries/surrealdb/injections.scm", "w")
 		io.output(injections_file)
 		io.write(injections_scm)
 		io.close(injections_file)
 	end
 end
-	
+
 return { setup = setup }
